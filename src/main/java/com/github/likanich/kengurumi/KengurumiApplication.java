704f06bae62b9ecf155ddcf2530c354720a1b2c1
Kamagorov Vasiliy <likanich152@gmail.com>
2021-04-28 15:13:24 +0700

    Added gradle

---
+++ src/main/java/com/github/likanich/kengurumi/KengurumiApplication.java
@@ -0,0 +1,13 @@
+package com.github.likanich.kengurumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KengurumiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KengurumiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/likanich/kengurumi/KengurumiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.likanich.kengurumi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KengurumiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

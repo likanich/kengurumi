2300c71da04cb376165b35f6d1723e31a9fb2641
Kamagorov Vasiliy <likanich152@gmail.com>
2021-05-07 21:57:20 +0700

    Added crochets on main page from database

--- src/main/java/com/github/likanich/kengurumi/controllers/MainController.java
+++ src/main/java/com/github/likanich/kengurumi/controllers/MainController.java
@@ -1,12 +1,22 @@
 package com.github.likanich.kengurumi.controllers;
 
+import com.github.likanich.kengurumi.repositories.CrochetRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
+    private final CrochetRepository crochetRepository;
+
+    public MainController(CrochetRepository crochetRepository) {
+        this.crochetRepository = crochetRepository;
+    }
+
     @GetMapping("/")
-    public String getCanvas() {
+    public String getCanvas(Model model) {
+        model.addAttribute("crochets", crochetRepository.findAll());
         return "index";
     }
 }

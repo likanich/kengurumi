56a10c8e34a6d101f70759287cddbc027b2ef40d
Kamagorov Vasiliy <likanich152@gmail.com>
2021-05-17 16:04:47 +0700

    add heroku config

--- src/main/java/com/github/likanich/kengurumi/config/DatabaseConfig.java
+++ src/main/java/com/github/likanich/kengurumi/config/DatabaseConfig.java
@@ -8,16 +8,16 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
-@Configuration
+//@Configuration
 public class DatabaseConfig {
 
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
-
-    @Bean
-    public DataSource dataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(dbUrl);
-        return new HikariDataSource(config);
-    }
+//    @Value("${spring.datasource.url}")
+//    private String dbUrl;
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        HikariConfig config = new HikariConfig();
+//        config.setJdbcUrl(dbUrl);
+//        return new HikariDataSource(config);
+//    }
 }
